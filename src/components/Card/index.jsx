e4df35673416a24d835be6e010763b529668583b
Paulo Vitor <pvitor7@hotmail.com>
2022-04-10 20:27:58 -0300

    cards coloridos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,12 +2,15 @@ import "./style.css";
 import { BsFillTrashFill } from "react-icons/bs";
 
 export default function Card({ description, type, value, deleteCard, id }) {
-  
   return (
     <div className={type}>
       <div className="card-title">
         <h2 className="card-description">{description}</h2>
-        <p className="card-value">R$ {value},00</p>{" "}
+        {type === "Despesa" ? (
+          <p className="card-value">R$ -{value},00 </p>
+        ) : (
+          <p className="card-value">R$ {value},00 </p>
+        )}
         <button onClick={() => deleteCard(id)} className="excluir">
           {" "}
           <BsFillTrashFill className="trash" size={20} />{" "}
